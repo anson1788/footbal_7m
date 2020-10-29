6a5d77169920cf976516cab474d49e9594e97f3f
IZJ6CH597FCA8YZ\jenkin <jenkin@home>
2020-10-29 20:12:23 +0800

    'd'

--- node/betChecker.js
+++ node/betChecker.js
@@ -3,25 +3,87 @@ let bfWinBetUtils = require('./class/bfWinBetUtils.js');
 
 let bcUtils = new basicUtils()
 let bfBetUtils = new bfWinBetUtils()
+let filterUtils = require('./class/dataFilter.js');
 
+var acct = require('./accountInfo.json');
+const TelegramBot = require('node-telegram-bot-api');
+var token = '';
+var fs = require('fs');
 
 async function init(){
 
     var liveUrl = "http://live.win007.com/indexall_big.aspx"
     var dom = null
+    var log = ""
     try{
         dom = await bcUtils.getHttpDomAsyn(liveUrl,"") 
     }catch(e){
         console.log("mean error "+e)
-        return
+        return null
     }
     if(dom==null){
         console.log("get 7m data error")
-        return
+    }else{
+        var liveMatchList  = bfBetUtils.parseBFLiveMatch(dom)
+        var separateList  = bfBetUtils.filterOutImmediateList(liveMatchList)
+
+        log += "Total Match :"+liveMatchList.length +"\n"
+        var hkjcList = fs.readFileSync("liveData/hkjcMatchList.json");
+        hkjcList = JSON.parse(hkjcList)
+
+        var m20List = separateList[0]
+        var m7List = separateList[1]
+       
+        log += "m20 Match :"+m20List.length +"\n"
+        log += "m7 Match :"+m7List.length +"\n"
+
+        var hkjcCrtList = bfBetUtils.getCrtHKJCList(m7List,hkjcList)
+        log += "liveHKJC Match :"+hkjcCrtList.length +"\n"
+
+        crtOddList = await bfBetUtils.addOddData(hkjcCrtList,bcUtils)
+        let ftUtils = new filterUtils()
+        var targetData = ftUtils.oneGoalOdd(crtOddList)
+        console.table(targetData)
+        var msg = ""
+        for(var i=0;i<targetData.length;i++){
+            msg += targetData[i].home +" vs "+targetData[i].away + " "+" "+targetData[i].hkjcOdd + " 主 " + targetData[i].betOdd
+        }
+        if(msg!=""){
+            bot.sendMessage(tgChanelId,msg);
+        }
+        crtOddList = await bfBetUtils.addOddData(m20List,bcUtils)
+        var hkjcId = []
+        for(var i=0;i<crtOddList.length;i++){
+            if(typeof(crtOddList[i].OddData) !="undefined"  &&
+               typeof(crtOddList[i].OddData[0]["香港马会"])!=="undefined"){
+                hkjcId.push(crtOddList[i].id)
+            }
+        }
+        log += "futureHKJC Match :"+hkjcId.length +"\n"
+       fs.writeFileSync("liveData/hkjcMatchList.json", JSON.stringify(hkjcId,null,2));
+       
+        /*
+        var list = await bfBetUtils.addOddData(liveMatchList,bcUtils)
+        liveMatchList = list
+        console.table(liveMatchList)
+        let ftUtils = new filterUtils()
+        var targetData = ftUtils.lookUpTwoMatch(liveMatchList)
+        console.table(targetData)
+        */
+       bot.sendMessage(tgLogChannel,log+"----------");
     }
-    var LiveMatchList  = bfBetUtils.parseBFLiveMatch(dom)
-    console.log(JSON.stringify(LiveMatchList,null,2))
 }
 
+if(typeof(acct.tgToken) !== 'undefined'){
+    token = acct.tgToken
+}
 
+var tgChanelId = ""
+var tgLogChannel = ""
+if(typeof(acct.tgMainChanelId) !== 'undefined'){
+    tgChanelId = acct.tgMainChanelId
+    tgLogChannel = acct.tgLogChanelId
+}
+
+bot = new TelegramBot(token, {polling: false});
 init()
