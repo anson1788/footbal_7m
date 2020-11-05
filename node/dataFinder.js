3295c842004262d7e612083953e11a083628d4c5
Anson Yu <cppany@cathaypacific.com>
2020-11-05 21:52:34 +0800

    no message

--- node/dataFinder.js
+++ node/dataFinder.js
@@ -10,12 +10,17 @@ function init(){
     //var targetData = ftUtils.lowHalfAndUp(dataList)
    // var targetData = ftUtils.nochangeOdd(dataList)
    //var targetData = ftUtils.nochangeOddn(dataList)
-   var targetData = ftUtils.samePointOddSwitchHomeDown(dataList)
+   //var targetData = ftUtils.samePointOddSwitchHomeDown(dataList)
    //var targetData = ftUtils.halfTohalf(dataList)
   // var targetData = ftUtils.highOdd(dataList)
    //var targetData = ftUtils.similarOdd(dataList)
    
+   var targetData = ftUtils.pointDropOdDrop(dataList)
+   var calculator = ftUtils.calculateResultAsianOdd(targetData)
+   var resultList = calculator[0]
+   var resultStat = calculator[1]
    console.table(targetData)
+   console.table([resultStat])
 }
 
 
