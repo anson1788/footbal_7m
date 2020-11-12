d613cba335c4aea5b5e34cd0ae17278f63a9cffd
iMacAnson <anson1788@gmail.com>
2020-11-13 03:22:43 +0800

    add daily data

---
+++ node/hkjcBetTester.js
@@ -0,0 +1,24 @@
+﻿
+
+let hkjcBetEngine = require('./class/hkjcBetEngine.js');
+var fs = require('fs');
+
+var acct = require('./accountInfo.json');
+let hkjcBE = new hkjcBetEngine()
+
+/*
+http://bf.win007.com/football/big/Over_20201022.htm
+
+*/
+
+async function init(){
+    await hkjcBE.buyOdd([{
+        "home":"塞爾維亞",
+        "away":"蘇格蘭",
+        "place":"主"
+    }],
+    acct
+    )
+}
+
+init()
